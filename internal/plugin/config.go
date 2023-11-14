94f676dd30121d05ea21732f6cb62f89efc5177a
storezhang <storezhang@gmail.com>
2023-11-14 15:27:59 +0800

    fix(配置): 强制只能使用loki类型

--- internal/plugin/config.go
+++ internal/plugin/config.go
@@ -11,7 +11,7 @@ type Config struct {
 	Level string `default:"debug" json:"level" yaml:"level" xml:"level" toml:"level" validate:"oneof=debug info warn error fatal"`
 	// 类型
 	// nolint: lll
-	Type core.Type `default:"zap" json:"type" yaml:"type" xml:"type" toml:"type" validate:"required,oneof=loki"`
+	Type core.Type `json:"type" yaml:"type" xml:"type" toml:"type" validate:"required,oneof=loki"`
 	// 日志调用方法过滤层级
 	Skip int `default:"2" json:"skip" yaml:"skip" xml:"skip" toml:"skip"`
 	// 调用堆栈层级
